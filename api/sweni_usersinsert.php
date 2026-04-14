4fca6b74027da14bd50e5277a3725dcc8379cb1e
Sweni Bhut <swenibhut@gmail.com>
2026-04-14 15:40:28 +0530

    Add files via upload

---
+++ api/sweni_usersinsert.php
@@ -0,0 +1,23 @@
+<?php
+include 'connect.php';
+
+$name  = mysqli_real_escape_string($con, $_POST["name"] ?? '');
+$email = mysqli_real_escape_string($con, $_POST["email"] ?? '');
+$password = mysqli_real_escape_string($con, $_POST["password"] ?? '');
+
+if ($name == "" || $email == "" || $password == "") {
+    echo "All fields are required.";
+    exit;
+}
+
+$sql = "INSERT INTO sweni_users (name, email, password) 
+        VALUES ('$name', '$email', '$password')";
+
+if(mysqli_query($con, $sql)){
+    echo "Record inserted successfully";
+} else {
+    echo "Error: " . mysqli_error($con);
+}
+
+mysqli_close($con);
+?>
